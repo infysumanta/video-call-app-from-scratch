74b9f74a8e0ae41cb7ba8087b013f544da59f9ef
Sumanta Kabiraj <sumantablog@gmail.com>
2023-06-04 00:01:03 +0530

    configure server handler

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+
+io.on('connection', (socket) => {
+  socket.on('join', () => {
+    socket.join('video-call');
+    console.log('user joined');
+  });
+
+  socket.on('offer', (offer) => {
+    socket.to('video-call').emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.to('video-call').emit('answer', answer);
+  });
+
+  socket.on('disconnected', () => {
+    socket.leave('video-call');
+    console.log('user disconnected');
+  });
+});
 
 http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
